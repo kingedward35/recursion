aa13945691851f24d79db6296ca866463dd98a27
Verlon Smith <verlon_s@yahoo.com>
2015-08-03 19:35:22 -0600

    Commit

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,46 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className) {
+
+  var output = [];
+  var findElement = function(element) {
+
+    if (element.classList) {
+      if((element.classList).contains(className)) {
+        output.push(element);
+      }
+    }
+
+    if(element.childNodes.length > 0) {
+      for(var i = 0; i < element.childNodes.length; i += 1) {
+        findElement(element.childNodes[i]);
+        }
+      } 
+    };
+    findElement(document.body);
+    return output;
 };
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,94 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function(object) {
+  var quotes = '"';
+  var arr = [];
+
+
+  if (typeof object === undefined || typeof object === 'function') {
+    return null;
+  }
+
+  if (typeof object === 'number') {
+    return object.toString();
+  }
+  
+  if (typeof object === 'boolean') {
+    return object.toString();
+  }
+
+  if (typeof object === null) {
+    return object.toString();
+  }
+
+  if (typeof object === 'string') {
+    return (quotes + object + quotes);
+  }
+  
+  if (Array.isArray(object)) {
+    var arr2 = [];
+    for (var i = 0; i < object.length; i += 1) {
+      arr2.push(stringifyJSON(object[i]));
+    }
+
+    return '[' + arr2.join(',') + ']';
+  }
+  
+  if (typeof object === 'object') {
+    var arr3 = [];
+    for (var key in object) {
+      if (stringifyJSON(object[key])) {
+      arr3.push(quotes + key + quotes + ':' + stringifyJSON(object[key]));
+    }
+  }
+  
+  return '{' + arr3 + '}';
+  }
+
 };
+
+
+// var stringifyJSON = function(obj) {
+//     var str = "";
+//     var q = '"';    
+//     if (obj === undefined || typeof obj === 'function'){
+//         return null;
+//     }
+//     else if (typeof obj === 'boolean' || typeof obj === 'number' || obj === null){
+//         return str += String(obj);
+//     } 
+//     else if (typeof obj === 'string'){
+//         return q + obj + q;
+//     }
+//     else if (Array.isArray(obj)) {
+//       var newArray = [];
+//       for (var i = 0; i < obj.length; i++){
+//         newArray.push(stringifyJSON(obj[i]));
+//       }
+//         return "[" + newArray.join(',') + "]";  
+//     } else {
+//         var objectKeys = [];
+//         for(var key in obj){
+//             if (stringifyJSON(obj[key])) {
+//                 objectKeys.push(q + key + q + ':' + stringifyJSON(obj[key]));
+//             }
+//         }
+//         return '{' + objectKeys + '}';
+//     }     
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
