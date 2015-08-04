1c964df77077f7638421e2869c825cf370705f4c
Verlon Smith <verlon_s@yahoo.com>
2015-08-03 19:58:46 -0600

    Commit

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,91 +6,29 @@
 var stringifyJSON = function(object) {
   var quotes = '"';
   var arr = [];
+  var str = '';
 
-
-  if (typeof object === undefined || typeof object === 'function') {
+  if (object === undefined || typeof object === 'function') {
     return null;
-  }
-
-  if (typeof object === 'number') {
-    return object.toString();
-  }
-  
-  if (typeof object === 'boolean') {
-    return object.toString();
-  }
-
-  if (typeof object === null) {
-    return object.toString();
-  }
-
-  if (typeof object === 'string') {
-    return (quotes + object + quotes);
-  }
-  
-  if (Array.isArray(object)) {
-    var arr2 = [];
+  } else if (typeof object === 'number' || typeof object === 'boolean' || object === null) {
+    return str += String(object);
+  } else if (typeof object === 'string') {
+     return (quotes + object + quotes);
+  } else if (Array.isArray(object)) {
     for (var i = 0; i < object.length; i += 1) {
-      arr2.push(stringifyJSON(object[i]));
+      arr.push(stringifyJSON(object[i]));
     }
 
-    return '[' + arr2.join(',') + ']';
-  }
-  
-  if (typeof object === 'object') {
-    var arr3 = [];
+    return '[' + arr.join(',') + ']';
+  } else {
+    var arr2 = [];
     for (var key in object) {
       if (stringifyJSON(object[key])) {
-      arr3.push(quotes + key + quotes + ':' + stringifyJSON(object[key]));
+      arr2.push(quotes + key + quotes + ':' + stringifyJSON(object[key]));
     }
   }
-  
-  return '{' + arr3 + '}';
+
+  return '{' + arr2 + '}';
   }
-
 };
-
-
-// var stringifyJSON = function(obj) {
-//     var str = "";
-//     var q = '"';    
-//     if (obj === undefined || typeof obj === 'function'){
-//         return null;
-//     }
-//     else if (typeof obj === 'boolean' || typeof obj === 'number' || obj === null){
-//         return str += String(obj);
-//     } 
-//     else if (typeof obj === 'string'){
-//         return q + obj + q;
-//     }
-//     else if (Array.isArray(obj)) {
-//       var newArray = [];
-//       for (var i = 0; i < obj.length; i++){
-//         newArray.push(stringifyJSON(obj[i]));
-//       }
-//         return "[" + newArray.join(',') + "]";  
-//     } else {
-//         var objectKeys = [];
-//         for(var key in obj){
-//             if (stringifyJSON(obj[key])) {
-//                 objectKeys.push(q + key + q + ':' + stringifyJSON(obj[key]));
-//             }
-//         }
-//         return '{' + objectKeys + '}';
-//     }     
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
